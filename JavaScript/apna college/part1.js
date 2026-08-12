a6a5dd2d6cf8f7b5920b501338952975f38a979c
Tushar Gade <110712003+tushargade01@users.noreply.github.com>
2026-08-12 19:10:51 +0530

    Implement divisibility check for a number
    Add a script to check if a number is divisible by 10.

---
+++ JavaScript/apna college/part1.js
@@ -0,0 +1,7 @@
+//q1 create a number variable num with some value.now, print "good" if the number is divisible by 10 and print "bad" if it is not.
+let num = 248;
+if (num % 10 == 0) {
+    console.log(`good, the number ${num} is divisible by 10`);
+} else {
+    console.log('bad');
+}
